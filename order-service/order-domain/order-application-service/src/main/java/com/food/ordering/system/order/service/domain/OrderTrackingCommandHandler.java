4de88484e2208125c1d1b242c006166578c4e172
Martin <martin@niceday.io>
2024-12-19 10:29:19 +0900

    OrderTrackingCommandHandler 구현

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackingCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackingCommandHandler.java
@@ -2,14 +2,35 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDateMapper;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OrderTrackingCommandHandler {
 
-	public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+	private final OrderDateMapper orderDateMapper;
+	private final OrderRepository orderRepository;
 
+	@Transactional(readOnly = true)
+	public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+		Optional<Order> orderResult = orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()));
+
+		if(orderResult.isEmpty()) {
+			log.warn("Could not find order with trakcing id: {}", trackOrderQuery.getOrderTrackingId());
+			throw new OrderNotFoundException("Could not find order with trakcing id:"
+					+ trackOrderQuery.getOrderTrackingId());
+		}
+		return orderDateMapper.orderToTrackOrderResponse(orderResult.get());
 	}
 }

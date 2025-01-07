0ad90fbcac478246334c7cacbb6afc19567a6f7c
lasan97 <fktks97@naver.com>
2025-01-08 01:07:40 +0900

    customer service application 생성

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer.customers(id, username, first_name, last_name) VALUES
+('d215b5f8-0249-4dc5-89a351fd148cfb41', 'user_1', 'First', 'User'),
+('d215b5f8-0249-4dc5-89a351fd148cfb42', 'user_2', 'Second', 'User');
